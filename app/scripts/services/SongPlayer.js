3d403e2ae0af3694b264dcdde66351ddb65c14ff
Brooke Bogdan <brookebogdan@Brookes-MacBook-Air.local>
2017-10-23 19:34:13 -0400

    updating player bar

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-  function SongPlayer(Fixtures) {
+  function SongPlayer($rootScope, Fixtures) {
     var SongPlayer = {};
 
     /**
@@ -32,6 +32,12 @@
         preload: true
       });
 
+      currentBuzzObject.bind('timeupdate', function() {
+          $rootScope.$apply(function() {
+              SongPlayer.currentTime = currentBuzzObject.getTime();
+          });
+      });
+
       SongPlayer.currentSong = song;
     };
 
@@ -61,6 +67,11 @@
     // */
 
     SongPlayer.currentSong = null;
+    /**
+    * @desc Current playback time (in seconds) of currently playing song
+    * @type {Number}
+    */
+    SongPlayer.currentTime = null;
 
     SongPlayer.play = function(song) {
       song = song || this.currentSong;
@@ -114,10 +125,21 @@
       }
     };
 
+    /**
+    * @function setCurrentTime
+    * @desc Sets the current time in current song
+    * @param {Number} time
+    */
+    SongPlayer.setCurrentTime = function(time) {
+        if (currentBuzzObject) {
+            currentBuzzObject.setTime(time);
+        }
+    };
+
     return SongPlayer;
   }
 
   angular
   .module('blocJams')
-  .factory('SongPlayer', ['Fixtures', SongPlayer]);
+  .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
